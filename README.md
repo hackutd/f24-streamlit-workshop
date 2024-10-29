164863b770e98ca074dd722db88a428436068492
veermshah <veermickey@gmail.com>
2024-10-29 12:34:28 -0500

    starter

---
+++ README.md
@@ -0,0 +1,9 @@
+# HackUTD 2024 Streamlit Workshop
+You can find the slides and notion page for this workshop here:
+https://hackutd.notion.site/Fall-Workshop-Series-HackUTD-Ripple-Effect-41be59ac980a47d88d9ae7c3eb9504b6
+
+
+# Streamlit
+Home Website: https://streamlit.io/
+Cheat sheet: https://docs.streamlit.io/develop/quick-reference/cheat-sheet
+Documentation: https://docs.streamlit.io/
---
+++ main.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import plost
+
+# Function to create and save weather counts to CSV
+def create_csv():
+    weather_counts = seattle_weather['weather'].value_counts()
+    weather_counts_df = pd.DataFrame(weather_counts).reset_index()
+    weather_counts_df.columns = ['weather', 'count']
+    weather_counts_df.to_csv('weather_counts.csv', index=False)
+     
+# Set page config
+### STEP 0 ###
+st.set_page_config(layout="wide")
+st.title(":rain_cloud: Weather Dashboard")
+
+# Sidebar settings
+st.sidebar.title('Settings')
+### STEP 1 ###
+
+# Metrics
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+### STEP 2 ###
+
+# Load weather data
+seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
+create_csv()
+weather_counts_csv = pd.read_csv("weather_counts.csv")
+
+# Charts
+c1, c2 = st.columns((7,3))
+with c1:
+    # Line Chart
+    ### STEP 3 ###
+    pass
+
+with c2:
+    # Donut Chart
+    ### STEP 4 ###
+    pass
